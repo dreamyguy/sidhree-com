331d3120c15ecf876018405b937d7316454e9296
dreamyguy <i@dreamyguy.com>
2016-02-14 16:52:53 +0100

    Let there be light

---
+++ gulp/options/jade.js
@@ -0,0 +1,31 @@
+var jadeOptions = {
+  pretty: true,
+  locals: {
+    name: "Wallace Sidhrée",
+    role: "Frontend Developer",
+    homeUrl: "http://sidhree.com",
+    locale: "en_US",
+    localeSite: "en",
+    id: "dreamyguy",
+    description: "Frontend Developer established in Oslo, Norway",
+    image: "/dist/img/sidhree-logo.png",
+    imageType: "image/png",
+    imageWidth: "1200",
+    imageHeight: "1200",
+    image2: "/dist/img/sidhree-logo-alt.png",
+    imageType2: "image/png",
+    imageWidth2: "1200",
+    imageHeight2: "620",
+    image3: "/dist/img/wallace-sidhree.png",
+    imageType3: "image/png",
+    imageWidth3: "900",
+    imageHeight3: "900",
+    imageFluidIcon: "/dist/img/fluidicon.png",
+    twitterCard: "summary_large_image",
+    googleSiteVerification: "",
+    googleAnalytics: "",
+    repo: "https://github.com/dreamyguy/sidhree-com"
+  }
+};
+
+module.exports = jadeOptions;
---
+++ src/base/js/init.js
@@ -0,0 +1,4 @@
+// docready for modern browsers
+// document.addEventListener("DOMContentLoaded", function(event) {
+//     console.log('base js blocking');
+// });
