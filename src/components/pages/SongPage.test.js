7ea23819590594b0587379514b4838921eee59cb
Wallace Sidhrée <i@dreamyguy.com>
2020-08-14 16:55:10 +0200

    :umbrella: Correct page reference so that page test will pass

--- src/components/pages/SongPage.test.js
+++ src/components/pages/SongPage.test.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import HomePage from "./HomePage";
+import SongPage from "./SongPage";
 
 test("renders heading", () => {
-  const { getAllByText } = render(<HomePage />);
+  const { getAllByText } = render(<SongPage />);
   const heading = getAllByText(/Wallace Sidhrée/i)[0];
   expect(heading).toBeInTheDocument();
   expect(heading).toHaveClass(
